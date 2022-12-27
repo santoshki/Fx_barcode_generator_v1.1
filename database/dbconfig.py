ddc54ca65f3c28a8efe8ab5449cbe8bbd5b71094
santoshki <santoshkdevp@gmail.com>
2022-12-27 21:55:55 +0530

    Added and updated db files and functions.

---
+++ database/dbconfig.py
@@ -0,0 +1,37 @@
+import sqlite3
+import pathlib
+
+current_dir = pathlib.Path(__file__).parent
+
+
+def db_insert(book_category_value, sequence_count_value):
+    db_name = str(book_category_value) + ".db"
+    print("dbname:", db_name)
+    print("Sequence count value:", sequence_count_value)
+    db_path = str(current_dir)
+    conn = sqlite3.connect(db_path + "\\" + db_name)
+    cursor = conn.cursor()
+    try:
+        cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='sequence_counter_value' ''')
+        if cursor.fetchone()[0] == 0:
+            table_create = """CREATE TABLE sequence_counter_value(COUNT TEXT)"""
+            cursor.execute(table_create)
+            conn.commit()
+            print("Table created in SQLlite DB.")
+        #c.execute('UPDATE abc SET logintime = ? WHERE useremail = ?', (now, email))
+        cursor.execute("INSERT INTO sequence_counter_value(COUNT)" "VALUES(?)", str(sequence_count_value))
+        #cursor.execute("UPDATE sequence_counter_value SET COUNT = ?", str(sequence_count_value))
+        conn.commit()
+        print("Data inserted/updated in SQL lite db")
+        data = cursor.execute('''SELECT * FROM sequence_counter_value''')
+        for row in data:
+            print(row)
+    except Exception as e:
+        print("Exception occurred in dbconfig:", e)
+        return 0
+
+
+if __name__ == '__main__':
+    book_category = "Kids"
+    sequence_count_value = "0"
+    db_insert(book_category, sequence_count_value)
--- usecase/actions.py
+++ usecase/actions.py
@@ -1,56 +1,56 @@
 from barcode import EAN13
 from barcode.writer import ImageWriter
 from Interface import actioninterface
+from database import dbconfig, dbread
+
 
 def generate_barcode(society_names_dropdown_value, book_category_dropdown_value, barcode_count_value):
-    print("Generating barcode(s)..")
-    book_identifier_sequence_value = "000"
-    if "Norwood" in society_names_dropdown_value:
-        society_name_phrase_value = "8917"
-    else:
-        society_name_phrase_value = "8910"
-    print("Mapping Dewey decimal values..")
+    sequence_count = dbread.db_read(book_category_dropdown_value)
+    print("Current sequence count:", sequence_count)
+    if sequence_count != -1:
+        #dbconfig.db_insert(book_category_dropdown_value, barcode_count_value)
+        new_sequence_count_value = sequence_count + int(barcode_count_value)
+        print("new sequence count value:", new_sequence_count_value)
 
-    if "Kids" in book_category_dropdown_value:
-        dewey_decimal_value = "000"
-    elif "Philosophy" in book_category_dropdown_value:
-        dewey_decimal_value = "001"
-    elif "Religion" in book_category_dropdown_value:
-        dewey_decimal_value = "002"
-    elif "Social Sciences" in book_category_dropdown_value:
-        dewey_decimal_value = "003"
-    elif "Language" in book_category_dropdown_value:
-        dewey_decimal_value = "004"
-    elif "Science" in book_category_dropdown_value:
-        dewey_decimal_value = "005"
-    if "Technology" in book_category_dropdown_value:
-        dewey_decimal_value = "006"
-    elif "Arts" in book_category_dropdown_value:
-        dewey_decimal_value = "007"
-    elif "Literature" in book_category_dropdown_value:
-        dewey_decimal_value = "008"
-    elif "History, Geography" in book_category_dropdown_value:
-        dewey_decimal_value = "009"
+        print("Generating barcode(s)..")
+        book_identifier_sequence_value = str(int(sequence_count)).zfill(3)
 
-    year_last_digits_value = actioninterface.get_cal_year()
-    for i in range(0, int(barcode_count_value)):
-        book_identifier_sequence_value = str(int(book_identifier_sequence_value) + 1).zfill(
-            len(book_identifier_sequence_value))
+        if "Norwood" in society_names_dropdown_value:
+            society_name_phrase_value = "8917"
+        else:
+            society_name_phrase_value = "8910"
+        print("Mapping Dewey decimal values..")
 
-        barcode_number = str(society_name_phrase_value) + str(year_last_digits_value) + str(dewey_decimal_value) + \
-                         str(book_identifier_sequence_value)
+        if "Kids" in book_category_dropdown_value:
+            dewey_decimal_value = "000"
+        elif "Philosophy" in book_category_dropdown_value:
+            dewey_decimal_value = "001"
+        elif "Religion" in book_category_dropdown_value:
+            dewey_decimal_value = "002"
+        elif "Social Sciences" in book_category_dropdown_value:
+            dewey_decimal_value = "003"
+        elif "Language" in book_category_dropdown_value:
+            dewey_decimal_value = "004"
+        elif "Science" in book_category_dropdown_value:
+            dewey_decimal_value = "005"
+        if "Technology" in book_category_dropdown_value:
+            dewey_decimal_value = "006"
+        elif "Arts" in book_category_dropdown_value:
+            dewey_decimal_value = "007"
+        elif "Literature" in book_category_dropdown_value:
+            dewey_decimal_value = "008"
+        elif "History, Geography" in book_category_dropdown_value:
+            dewey_decimal_value = "009"
 
-        print("Barcode number:", barcode_number)
-        generated_barcode = EAN13(str(barcode_number), writer=ImageWriter())
-        name = "barcode" + str(i)
-        generated_barcode.save(name)
-        print("Barcode" + str(i) + "generated and saved successfully.")
-
-
-
-
-
-
-
-
-
+        year_last_digits_value = actioninterface.get_cal_year()
+        for i in range(sequence_count, int(new_sequence_count_value)):
+            book_identifier_sequence_value = str(int(book_identifier_sequence_value) + 1).zfill(3)
+            barcode_number = str(society_name_phrase_value) + str(year_last_digits_value) + str(dewey_decimal_value) + \
+                             str(book_identifier_sequence_value)
+            print("Barcode number:", barcode_number)
+            generated_barcode = EAN13(str(barcode_number), writer=ImageWriter())
+            barcode_value = str(barcode_number)
+            generated_barcode.save(barcode_value)
+            print(str(i) + " Barcodes generated and saved successfully.")
+            print("new sequence count value:", new_sequence_count_value)
+        dbconfig.db_insert(book_category_dropdown_value, int(new_sequence_count_value))
